93b4db0cfa54410f5f36223debd2df54742338db
Luke Dillon <luke.k.dillon@gmail.com>
2019-05-10 15:34:14 -0700

    working build

---
+++ functions/index.js
@@ -0,0 +1,193 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+
+// Listens for new messages added to /messages/:pushId/original and creates an
+// uppercase version of the message to /messages/:pushId/uppercase
+
+function getWinner(a, b)
+{
+    console.log("Determining Winner...");
+    var winner;
+    if (a == b)
+    {
+        // std::cout << "we found a tie" << std::endl;
+        winner = "tie";
+        console.log("Tie!");
+        // logIt("Tie game with " + a + "and" + b + "!");
+        return winner;
+    }
+
+    switch(a)
+    {
+        case "rock":
+        {
+            if (b == "scissors" || b == "lizard") { winner = a; break; }
+            else { winner = b; break; }
+        }
+        case "paper":
+        {
+            if (b == "rock" || b == "spock") { winner = a; break; }
+            else { winner = b; break; }
+        }
+        case "scissors":
+        {
+            if (b == "paper" || b == "lizard") { winner = a; break; }
+            else { winner = b; break; }
+        }
+        case "lizard":
+        {
+            if (b == "paper" || b == "spock") { winner = a; break; }
+            else { winner = b; break; }
+        }
+        case "spock":
+        {
+            if (b == "rock" || b == "scissors") { winner = a; break; }
+            else { winner = b; break; }
+        }
+        case "tie":
+        {
+            break;
+        }
+    }
+    if (winner == a) 
+    {
+        console.log("You win with " + a + " over " + b + "!");
+        // logIt("You win with " + a + " over " + b + "!");
+    }
+    else
+    {
+        console.log("You lost with " + b + " over " + a + "!");
+        // logIt("You lost with " + b + " over " + a + "!");
+    }
+    return winner;
+}
+
+var globalWinner = "n";
+exports.getWinner = functions.database.ref('/Game/{gameID}/{user}/status').onWrite((change, context) => {
+    console.log("func triggered");
+    var msg = "!";
+    var msgCopy = "none";
+    // get username and gameID
+    const username = context.params.user;
+    const getGameID = context.params.gameID;
+
+    // if we have changed from played to not played
+    /*
+    if ((change.before.val() == "p") && change.after.val() == "np")
+    {
+        return admin.database().ref("Game/" + getGameID + "/" + username + "/msgBoard").update({
+            winner : winnerName,
+            winnerAvailable : winAvail,
+            message : "Winner = " + globalWinner
+        });
+    }
+    */
+    
+    
+    
+    var getOpponent = "";
+    var getStatus = change.after.val();
+    var getOpponentStatus = "";
+    var getHand = "";
+    var getOppHand = "";
+    var winAvail = "n";
+    
+    // get my opponent and my hand
+    admin.database().ref("Game/" + getGameID + "/" + username).on("value", function(snapshot) {
+        const mySnap = snapshot.val();
+        getOpponent = mySnap.opponent;
+        getHand = mySnap.hand;
+        getStatus = mySnap.status;
+    });
+
+    // get opponent's hand and status
+    admin.database().ref("Game/" + getGameID + "/" + getOpponent).on("value", function(snapshot) {
+        const mySnap = snapshot.val();
+        getOpponentStatus = mySnap.status;
+        getOppHand = mySnap.hand;
+    });
+
+    console.log("My name: " + username);
+    console.log("Opp name: " + getOpponent);
+    console.log("My status: " + getStatus);
+    console.log("Opp status: " + getOpponentStatus);
+    console.log("My hand: " + getHand);
+    console.log("Opp hand: " + getOppHand);
+
+    var winnerName = "nobody";
+    if (getOpponentStatus != "np" && getStatus != "np")
+    {
+        winAvail = "y";
+        const winner = getWinner(getHand, getOppHand);
+        globalWinner = winner;
+        console.log("Winner is: " + winner);
+        if (winner == getHand)
+        {
+            // then username wins
+            winnerName = username;
+        }
+        else if (winner == getOppHand)
+        {
+            // opp wins
+            winnerName = getOpponent;
+        }
+        else 
+        {
+            // tie
+            winnerName = "tie";
+        }
+
+        msg = "Winner is " + winner + "!";
+        // msgCopy = "Winner Available!";
+
+        
+        admin.database().ref("Game/" + getGameID + "/" + getOpponent + "/msgBoard").update({
+            winner : winnerName,
+            winnerAvailable : winAvail,
+            message : msg
+        });
+
+        admin.database().ref("Game/" + getGameID + "/" + username + "/msgBoard").update({
+            winner : winnerName,
+            winnerAvailable : winAvail,
+            message : msg
+        });
+        
+        /*
+        admin.database().ref("Game/" + getGameID + "/" + getOpponent).update({
+            status : "np"
+        });
+        
+        admin.database().ref("Game/" + getGameID + "/" + username).update({
+            status : "np"
+        });
+        */
+
+        return 1;
+    }
+    else
+    {
+        winAvail = "n";
+        msg = "!";
+        msgCopy = "none";
+        console.log("Both haven't played yet...");
+        return admin.database().ref("Game/" + getGameID + "/" + username + "/msgBoard").update({
+            winner : winnerName,
+            winnerAvailable : winAvail,
+            message : "!"
+        });
+    }
+
+    // You must return a Promise when performing asynchronous tasks inside a Functions such as
+    // writing to the Firebase Realtime Database.
+    // return snapshot.ref.parent.child('uppercase').set(uppercase);
+    
+});
--- main.js
+++ main.js
@@ -1,191 +1,242 @@
-var myUsername, myHand, getFirebaseOpp, getGame;
+// Globals
+var myUsername = "b"
+var myHand, getFirebaseOpp;
+var getGame = "b";
 var getOppStatus, getMyStatus, getOppHand;
-var myOpponent = "bob";
+var myOpponent;
+var gameCount = 0;
+var wins = 0;
+var losses = 0;
+var ties = 0;
+var interval;
+
+var clickEnabled = 0;
+var errorCounter = 0;
+var bothP = 0;
 firebase.database().ref("/Active/" + myUsername).set(null);
 firebase.database().ref("Game/").set(null);
+
+function enableImageClick()
+{
+    console.log("Enabling Image clicks...");
+    document.getElementById('rock').style.pointerEvents = 'auto'; 
+    document.getElementById('paper').style.pointerEvents = 'auto';
+    document.getElementById('scissors').style.pointerEvents = 'auto';
+    document.getElementById('lizard').style.pointerEvents = 'auto';
+    document.getElementById('spock').style.pointerEvents = 'auto'; 
+    gameCount += 1;
+    errorCounter = 0;
+    logIt("Game " + gameCount + " has begun!");
+    clickEnabled = 1;
+}
+
+// a useless function
+function sleep(milliseconds) 
+{
+    var start = new Date().getTime();
+    for (var i = 0; i < 1e7; i++) 
+    {
+        if ((new Date().getTime() - start) > milliseconds)
+        {
+            break;
+        }
+    }
+  }
 
 function enterUsername()
 {
     var usernameInput = document.getElementById('username').value;
     myUsername = String(usernameInput);
-    console.log(usernameInput);
+    console.log("Your username is " + usernameInput);
     firebase.database().ref("/Active/" + myUsername).set({
         username: myUsername,
         status: "np",
         hand: null
     });
+    logIt("Hi " + myUsername + "!");
+    enableImageClick();
+}
+
+function makeid(length) 
+{
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) 
+    {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+// link: https://stackoverflow.com/questions/18982642/how-to-disable-and-then-enable-onclick-event-on-div-with-javascript
+function disableImageClick()
+{
+    console.log("Disabling Image clicks...");
+    document.getElementById('rock').style.pointerEvents = 'none'; 
+    document.getElementById('paper').style.pointerEvents = 'none';
+    document.getElementById('scissors').style.pointerEvents = 'none';
+    document.getElementById('lizard').style.pointerEvents = 'none';
+    document.getElementById('spock').style.pointerEvents = 'none'; 
+    clickEnabled = 0;
+}
+disableImageClick();
+ 
+function playAudio(a) 
+{ 
+    var x = document.getElementById(a + "fx");
+    x.play(); 
+} 
+
+function pauseAudio() 
+{ 
+    x.pause(); 
+}
+
+function pickImage(aHand)
+{
+    disableImageClick();
+    console.log("You clicked " + aHand);
+    myHand = aHand;
+    logIt("You picked " + aHand + "!");
+    playAudio(myHand);
+
+    if (getGame == "b")
+    {
+        // get the game if you were invited
+        firebase.database().ref("Active/" + myUsername).on("value", function(snapshot) {
+            var snapVal = snapshot.val();
+            getGame = snapVal.gameRoom;
+            myOpponent = snapVal.opponent;
+            
+            // update the db with your hand
+            firebase.database().ref("Game/" + getGame + "/" + myUsername).update({
+                hand : aHand,
+                status : "p",
+            });
+        });
+    }
+
+    else
+    {
+        // update the db with your hand
+        firebase.database().ref("Game/" + getGame + "/" + myUsername).update({
+            hand : aHand,
+            status : "p",
+        });
+    } 
+
+    // now begin to look at firebase every 5s
+    interval = setInterval(checkForUpdates, 2000);
 }
 
 function pickOpponent()
 {
-    
-    /*
+    // get the input from the html
     myOpponent = document.getElementById('opponent').value;
-    console.log(myOpponent);
-    firebase.database().ref("Game/" + myOpponent + "vs" + myUsername).update({
+    console.log("you picked " + myOpponent);
+
+    // make a random game ID
+    getGame = makeid(5);
+    firebase.database().ref("Game/" + getGame).update({
         gameOn : "y"
     });
-    firebase.database().ref("Active/" + myOpponent + "/").update({
-        opponent : myUsername
-    });
-    firebase.database().ref("Active/" + myUsername + "/").update({
+
+    // add the game ID to my name
+    firebase.database().ref("Active/" + myUsername).update({
+        gameOn : "y",
+        gameRoom : getGame,
         opponent : myOpponent
     });
+
+    // add game ID to opponent
+    firebase.database().ref("Active/" + myOpponent).update({
+        gameOn : "y",
+        gameRoom : getGame,
+        opponent : myUsername
+    });
+
+    // update the opponent's opponent
+    firebase.database().ref("Game/" + getGame + "/" + myOpponent).update({
+        opponent : myUsername,
+        username : myOpponent,
+        status : "np"
+    });
+
+    // update my opponent
+    firebase.database().ref("Game/" + getGame + "/" + myUsername).update({
+        opponent : myOpponent,
+        username : myUsername,
+        status : "np"
+    });
     logIt(myUsername + " wants to play with " + myOpponent);
-    */
 }
 
-function uncheckBoxes()
+var lastMsg = "";
+var winnerQ = 0;
+var snaps = "e";
+function checkForUpdates()
 {
-    var radios = document.getElementsByName('hand');
-    for (var i = 0, length = radios.length; i < length; i++)
-    {
-        radios[i].disabled = false;
-    }
-    document.getElementById('pickButton').disabled = false;
-}
+    
+    // console.log("Checking for updates...");
+    firebase.database().ref("Game/" + getGame + "/" + myUsername + "/msgBoard/message").on("value", function(snapshot){
+        
+        snaps = snapshot.val();
+        console.log(myUsername + " " + snaps);
 
-function pickHand()
-{
-    var radios = document.getElementsByName('hand');
-    for (var i = 0, length = radios.length; i < length; i++)
-    {
-        if (radios[i].checked)
+        // winner is available
+        if (snaps.includes("Winner"))
         {
-            // alert(radios[i].value);
-            myHand = radios[i].value;
-            console.log(radios[i].value);
-            // only one radio can be logically checked, don't check the rest
-            break;
+            winnerQ = 1;
         }
-    }
-
-    firebase.database().ref("Active/" + myUsername + "/").update({
-        hand : myHand,
-        status: "p",
-        username: myUsername
     });
 
-    firebase.database().ref("Game/" + myUsername).update({
-        hand : myHand,
-        status : "p",
-        username : myUsername
-    });
-    for (var i = 0, length = radios.length; i < length; i++)
+    if (winnerQ && !clickEnabled)
     {
-        radios[i].disabled = true;
-    }
-    document.getElementById('pickButton').disabled = true;
-    logIt(myUsername + " picked a hand!");
-}
+        winnerQ = 0;
 
-firebase.database().ref("Active/").on("child_changed", function(snapshot) {
+        // no longer check for firebase updates
+        clearInterval(interval);
 
-    var snaps = snapshot.val();
-    console.log("Change: " + snaps.status);
-    if (snaps.username == myUsername)
-    {
-        console.log("It's me!");
-        if (snaps.status == "p")
+        // change status back to np
+        firebase.database().ref("Game/" + getGame + "/" + myUsername).update({
+            status : "np"
+        });
+        
+        if (snaps.includes(myHand))
         {
-            console.log("You played!");
+            wins += 1;
+            document.getElementById("wins").innerHTML = "Wins: " + wins;
+            logIt("You won!")
+            playAudio("win");
         }
-    }
-});
-
-setInterval(checkFirebase, 10000);
-function checkFirebase()
-{
-    firebase.database().ref("Game/").on("value", function(snapshot) {
-
-        var snaps = snapshot.val();
-        console.log(snaps);
-
-        // get opponent status and hand
-        getOppStatus = snaps[myOpponent].status;
-        getOppHand = snaps[myOpponent].hand;
-        console.log("Opponent Hand: " + getOppHand);
-
-        getMyStatus = snaps[myUsername].status;
-        console.log("Bob Status: " + getOppStatus);
-        if (getOppStatus == "p" && getMyStatus == "p")
+        else if (snaps.includes("tie"))
         {
-            console.log("You both played!");
-            getWinner(myHand, getOppHand);
-
-            // fix the page stuff
-            uncheckBoxes();
-
-            // change status
-            firebase.database().ref("Game/" + myUsername).update({
-                status : "np"
-            });
-            firebase.database().ref("Game/" + myOpponent).update({
-                status : "np"
-            });
+            ties += 1;
+            document.getElementById("ties").innerHTML = "Ties: " + ties;
+            logIt("You tied!");
         }
         else
         {
-            console.log("Waiting on someone to play...");
+            losses += 1;
+            document.getElementById("losses").innerHTML = "Losses: " + losses;
+            logIt("You lost!");
+            playAudio("lose");
         }
-    });
-}
+        enableImageClick();    
+    }
+    errorCounter += 1;
 
-function getWinner(a, b)
-{
-    var winner;
-    if (a == b)
+    // both are stuck on p
+    if (errorCounter >= 5)
     {
-        // std::cout << "we found a tie" << std::endl;
-        winner = "tie";
-        console.log("Tie!");
-        return winner;
+        console.log("inside error fixer!")
+        
+        // change to error, but it will fix itself
+        firebase.database().ref("Game/" + getGame + "/" + myOpponent).update({
+            status : "e"
+        });
     }
-
-    switch(a)
-    {
-        case "rock":
-        {
-            if (b == "scissors" || b == "lizard") { winner = a; break; }
-            else { winner = b; break; }
-        }
-        case "paper":
-        {
-            if (b == "rock" || b == "spock") { winner = a; break; }
-            else { winner = b; break; }
-        }
-        case "scissors":
-        {
-            if (b == "paper" || b == "lizard") { winner = a; break; }
-            else { winner = b; break; }
-        }
-        case "lizard":
-        {
-            if (b == "paper" || b == "spock") { winner = a; break; }
-            else { winner = b; break; }
-        }
-        case "spock":
-        {
-            if (b == "rock" || b == "scissors") { winner = a; break; }
-            else { winner = b; break; }
-        }
-        case "tie":
-        {
-            break;
-        }
-    }
-    if (winner == a) 
-    {
-        console.log("You win with " + a + " over " + b + "!");
-        logIt("You win with " + a + " over " + b + "!");
-    }
-    else
-    {
-        console.log("You lost with " + b + " over " + a + "!");
-        logIt("You lost with " + b + " over " + a + "!");
-    }
-    return winner;
-
 }
 
 // messages
@@ -198,5 +249,13 @@ function logIt(msg)
     list.insertBefore(newLog, list.childNodes[0]);  // Insert <li> before the first child of <ul>
 }
 
-
-    
+// unused
+function uncheckBoxes()
+{
+    var radios = document.getElementsByName('hand');
+    for (var i = 0, length = radios.length; i < length; i++)
+    {
+        radios[i].disabled = false;
+    }
+    document.getElementById('pickButton').disabled = false;
+}
